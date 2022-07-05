5b5141efc5065d9f893d9b37baccfebfe61f4d77
IshanPoudel <ishan.poudel@mavs.uta.edu>
2022-07-04 23:47:36 -0500

    imported requiremnets

---
+++ main.py
@@ -0,0 +1,8 @@
+#use the MINST fashion project for classification of projects
+
+import tensorflow
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data= keras.datasets.fashion_mnist
